0b9c7a6c8ccc2f059456eac8b986c5441becff61
nzoLogic <nzoLogic@gmail.com>
2017-01-06 11:01:30 -0800

    test

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,33 +5,49 @@
  */
 
 angular
-  .module('tunely', [])
-  .controller('AlbumsIndexController', AlbumsIndexController);
-  // ^ the first argument is a string naming the controller,
-  // the second argument is a function that defines the capacities
-  // of the controller.
+    .module('tunely', [])
+    .controller('AlbumsIndexController', AlbumsIndexController);
+// ^ the first argument is a string naming the controller,
+// the second argument is a function that defines the capacities
+// of the controller.
+AlbumsIndexController.$inject = ['$http'];
 
-function AlbumsIndexController () {
-  var vm = this;
-  vm.newAlbum = {};
+function AlbumsIndexController($http) {
+    var vm = this;
+    vm.newAlbum = {};
+    vm.newAlbum = {
+      artistName: vm.artistName,
+      name: vm.name,
+      releaseDate: vm.releaseDate,
+      genres: [ vm.genre ]
+    };
+    vm.albums = [];
+    $http({
+        method: 'GET',
+        url: '/api/albums'
+    }).then(successAlbums, handleError);
 
-  vm.newAlbum = {
-      name: 'Viva Hate',
-      artistName: 'Morrissey'
-  };
-
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
+    vm.createAlbum = function(){
+      console.log(vm.newAlbum);
+      $http({
+              method: 'POST',
+              url: '/api/albums',
+              data: vm.newAlbum
+          })
+      .then(successPost, handleError);
     }
-  ];
+    function successPost(res){
+      console.log(res);
+      vm.albums.push(res.data)
+    }
+
+    function successAlbums(res) {
+      console.log(res)
+        vm.albums = res.data;
+    }
+
+    function handleError(err) {
+        console.log('errr');
+    }
+
 }
